5b7328dad76799aaef82021b72538f84d3f822a0
jmargielewski <kuba.margielewski@gmail.com>
2019-03-12 09:26:56 +0000

    add header of page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from '../Header/Header';
+import Main from '../Main/Main';
+import './app.css';
 
-const App = () => <div>Hello world</div>;
+const App = () => (
+  <Router>
+    <div className="app">
+      <Header />
+      <Route path="/" component={Main} />
+    </div>
+  </Router>
+);
 
 export default App;
